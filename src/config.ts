553d4bb2572c13ef31b5de678d1c62f4a62f1908
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2022-07-02 13:16:36 +0100

    hedera: config params

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+let config = {
+  operator: {
+      accountId: "",
+      publicKey: "",
+      privateKey: ""
+  },
+  network: {
+    "0.testnet.hedera.com:50211": "0.0.3",
+    "1.testnet.hedera.com:50211": "0.0.4",
+    "2.testnet.hedera.com:50211": "0.0.5",
+    "3.testnet.hedera.com:50211": "0.0.6"
+  }
+}
